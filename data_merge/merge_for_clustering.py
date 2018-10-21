df7998d3803294e674fa080becc580947a2df998
goppy001 <tacumin.goppy.06@vesta.ocn.ne.jp>
2018-10-22 08:27:12 +0900

    バグ修正

--- data_merge/merge_for_clustering.py
+++ data_merge/merge_for_clustering.py
@@ -8,19 +8,25 @@ import glob
 import re
 import datetime
 
-exception_dir  = "./result"
 target_dir = "./"
 headerlist = []
 datalist = []
 tmp_list = []
-d = 91
-d_idx = d-1
+cluster_file = "for_clustering.csv"
+exception_dir = "./result"
 
-#resultディレクトリ存在チェック作成
-if os.path.isdir(exception_dir):
-    pass
-else:
-    os.mkdir(exception_dir)
+# ディレクトリ存在チェック
+
+
+def check_dir(dirpath):
+    if os.path.isdir(dirpath):
+        pass
+    else:
+        os.mkdir(dirpath)
+
+
+check_dir(exception_dir)
+
 
 # 特定のディレクトリを対象外としてリストを構成する
 target_files = []
@@ -42,14 +48,15 @@ for csvfile in target_files:
         target_list.append(csvfile)
 
 dir_list = []
-#各CSVをひとつのCSVにまとめて出力する
+
+# 次元数調整（次元が小さい日時データはクラスタリングの対象外とする）
 for i, file in enumerate(target_list):
     if size_list[i] < max(size_list):
         continue
     else:
         name, _ = os.path.splitext(os.path.basename(file))
-        name    = name.replace("trend_cpu_", "")
-        rename    = pd.to_datetime(name, format='%Y-%m-%d')
+        name = name.replace("trend_cpu_", "")
+        rename = pd.to_datetime(name, format='%Y-%m-%d')
         rename_pattern = "(.*) (.*)"
         d = re.search(str(rename_pattern), str(rename))
         dd = d.group(1).split("-")
@@ -60,17 +67,17 @@ for i, file in enumerate(target_list):
         dir_list = sorted(dir_list)
         tmp_list.append(d.group(1))
         tmp_list = sorted(tmp_list)
-        
+
 re_list = []
 re_dir_list = []
-#上記ソートだけではだめなので文字型の日付をdatetime型に変換してリストに再代入
+# 上記ソートだけではだめなので文字型の日付をdatetime型に変換してリストに再代入
 for file in tmp_list:
     name = pd.to_datetime(file, format='%Y-%m-%d')
     rename = '{d.year}-{d.month}-{d.day}'.format(d=name)
     re_list.append(rename)
 
 
-#ディレクトリ名をソート
+# ディレクトリ名をソート
 for file in dir_list:
     name = file.replace("./", "")
     name = pd.to_datetime(name, format='%Y-%m')
@@ -78,42 +85,34 @@ for file in dir_list:
     re_dir_list.append(rename)
 red_dir_list = sorted(re_dir_list)
 
-target_files = ["./" + re_dir_list[i] + "/trend_cpu_" + str(tf) + ".csv" for i, tf in enumerate(re_list)]
+target_files = ["./" + re_dir_list[i] + "/trend_cpu_" +
+                str(tf) + ".csv" for i, tf in enumerate(re_list)]
 
+
+##########################################################
+###メイン処理###
+##########################################################
 for i, f in enumerate(target_files):
     df = pd.read_csv(f, index_col=0)
-    time = df.iloc[:,0].values
-    data = df.iloc[:,1].values
+    time = df.iloc[:, 0].values
+    data = df.iloc[:, 1].values
     name, _ = os.path.splitext(os.path.basename(f))
-    name    = name.replace("trend_cpu_", "")
-    rename    = pd.to_datetime(name, format='%Y-%m-%d')
+    name = name.replace("trend_cpu_", "")
+    rename = pd.to_datetime(name, format='%Y-%m-%d')
     rename_pattern = "(.*) (.*)"
     d = re.search(str(rename_pattern), str(rename))
     headerlist.append(d.group(1))
-    #最初のループでインデックスとなる時刻と時刻ヘッダを取り出す→別配列に格納
+    # 最初のループでインデックスとなる時刻と時刻ヘッダを取り出す→別配列に格納
     if i == 0:
-        headerlist.insert(0,"TIME")
-        datalist.append(pd.DataFrame(time))
-        num_na = df.isnull().sum()
-        #今後移動平均の範囲が変わった時ようにNaの数を数えるようにしている
-        #このNaを前日データで埋めるため
-        num_na = num_na.iloc[1].astype(int)
-        prev_data=df.iloc[:,1].iloc[-num_na:]
-        prev_data.reset_index()
-        df_data = pd.DataFrame(data)
-    if i > 0:
-        df_data  = pd.concat([prev_data,df.iloc[:,1]], ignore_index=True).dropna()
-        df_data  = df_data.values
-        prev_data = df.iloc[:,1].iloc[-num_na:]
-        prev_data.reset_index()
-        df_data = pd.DataFrame(df_data)
-    datalist.append(df_data)
-    if i == d_idx:
-        break
-    
+        headerlist.insert(0, "TIME")
+        time = pd.DataFrame(time)
+        datalist.append(time)
+    data = pd.DataFrame(data)
+    datalist.append(data)
+
 df = pd.concat(datalist, axis=1)
 df.columns = headerlist
 
 df = pd.DataFrame(df)
 df.sort_index(axis=1, ascending=False, inplace=True)
-df.to_csv("default_learn.csv")
+df.to_csv(cluster_file)
---
+++ dtw.py
@@ -0,0 +1,95 @@
+#!/home/glodia/.pyenv/versions/3.6.5/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import glob
+from itertools import product
+import re
+import shutil
+
+def check_dir(dirpath):
+    if os.path.isdir(dirpath):
+        pass
+    else:
+        os.mkdir(dirpath)
+
+target_dir = "./"
+result_dir = "./result"
+exception_dir = result_dir
+check_dir(result_dir)
+
+work_dir = "dtw"
+work_dir = result_dir + "/" + work_dir
+check_dir(work_dir)
+dir_png = "png"
+dir_png = work_dir + "/" + dir_png
+check_dir(dir_png)
+
+
+#####################################################
+###メイン処理####
+#####################################################
+
+delta = lambda a, b: (a - b)** 2
+first = lambda x: x[0]
+second = lambda x: x[1]
+
+#最小値を算出するための関数
+def minval(val1, val2, val3):
+    if first(val1) <= min(first(val2), first(val3)):
+        return val1, 0
+    elif first(val2) <= first(val3):
+        return val2, 1
+    else:
+        return val3,2
+
+#dtw距離の計算
+def calc_dtw(A, B):
+    S = len(A)
+    T = len(B)
+    m = [[0 for j in range(T)] for i in range(S)]
+    m[0][0] = (delta(A[0], B[0]), (-1, -1))
+    for i in range(1, S):
+        m[i][0] = (m[i - 1][0][0] + delta(A[i], B[0]), (i - 1, 0))
+    for j in range(1,T):
+        m[0][j] = (m[0][j-1][0] + delta(A[0], B[j]), (0,j-1))
+
+    for i in range(1,S):
+        for j in range(1,T):
+            minimum, index = minval(m[i-1][j], m[i][j-1], m[i-1][j-1])
+            indexes = [(i-1,j), (i,j-1), (i-1,j-1)]
+            m[i][j] = (first(minimum) + delta(A[i], B[j]), indexes[index])
+    return m
+
+
+target_file = "for_clustering.csv"
+df = pd.read_csv(target_file, index_col=[0])
+name, _ = os.path.splitext(os.path.basename(target_file))
+df_data = df.iloc[:, 1:]
+df_data = df_data.dropna()
+dtw = []
+for i, (key1, key2) in enumerate(product(df_data.columns, df_data.columns)):
+    dtw.append(calc_dtw(df_data[key1].get_values(),df_data[key2].get_values())[-1][-1][0])
+    print(key1 + " - " + key2, dtw[i])
+
+dtw = np.array(dtw).reshape(len(df_data.columns), -1)
+
+fig = plt.figure(figsize=(20,20))
+ax = fig.add_subplot(111)
+ax.set_aspect("equal")
+plt.pcolor(dtw, cmap=plt.cm.Blues)
+plt.xlim(xmax=len(df.columns)-1)
+plt.ylim(ymax=len(df.columns)-1)
+plt.xticks(np.arange(0.5, len(df.columns)+0.5), list(df.columns), rotation=90)
+plt.yticks(np.arange(0.5, len(df.columns)+0.5), list(df.columns))
+plt.colorbar()
+plt.savefig(dir_png + "/dtw_" + name + ".png")
+plt.close()
+
+
+shutil.move(target_file, work_dir)
